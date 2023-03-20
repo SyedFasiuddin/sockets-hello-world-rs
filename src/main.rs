063d02dc8964ccecb5ec3684ae7269edad1876fc
Syed Fasiuddin <66054777+SyedFasiuddin@users.noreply.github.com>
2023-03-20 22:44:21 +0530

    feat: socket file communication (one way)
    - create a socket file
    - allocate a smol buffer
    - read the data sent into that buffer
    - on successful read, print the contents of buffer as string.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::os::unix::net::UnixListener;
+use std::io::Read;
+
 fn main() {
-    println!("Hello, world!");
+    let listener = match UnixListener::bind("/tmp/socket-rs-hello.socket") {
+        Ok(socket) => socket,
+        Err(e) => { eprintln!("Failed to create the socket due to: {e}"); std::process::exit(1); }
+    };
+
+    let mut buffer = [0; 20];
+    match listener.accept() {
+        Ok((mut socket, addr)) => {
+            match socket.read(&mut buffer) {
+                Ok(size) => print!("Size: {size}\nMsg: {}", String::from_utf8(buffer.to_vec()).unwrap()),
+                Err(e) => eprintln!("Failed to read from socket (addr: {addr:?}) into buffer due to: {e}"),
+            }
+        },
+        Err(e) => eprintln!("Failed to accept connection due to: {e}"),
+    }
 }
