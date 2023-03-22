c3df77f876e78f230970b21d4f247e2982086d98
Syed Fasiuddin <66054777+SyedFasiuddin@users.noreply.github.com>
2023-03-22 19:39:20 +0530

    feat: simple chat app
    - making a server and client implementation
    - both server and client can send and receive data over a connection
    - the server waits for connection
    - the client connects to the server
    
    Limitation:
    1. Messages can only be sent one after another, first will always be the
       client and then the server.
    2. The client, or server, can't send messages back to back.
    3. First the client sends a message till it is received the server is
       blocked, once the message is received the server is blocked on
       reading the input and client is then blocked, after the server reads
       input it then sends it to the client only then the client is freed to
       read from the keyboard, this goes on forever.

--- src/main.rs
+++ src/main.rs
@@ -78,6 +78,83 @@ fn socket_file() {
     }
 }
 
-fn main() {
+#[allow(dead_code)]
+fn chat_client() {
+    use std::net::TcpStream;
+    use std::io::{Write, Read};
 
+    match TcpStream::connect("127.0.0.1:8000") {
+        Ok(mut stream) => {
+            println!("Connected to: {}", stream.peer_addr().unwrap());
+            let mut buffer = [0; 100];
+            let mut input = String::new();
+            loop {
+                match std::io::stdin().read_line(&mut input) {
+                    Ok(size) => {
+                        println!("Read: {size} bytes");
+
+                        let size = stream.write(input.as_bytes()).unwrap();
+                        println!("Sent {size} bytes");
+                        input.clear();
+
+                        let size = stream.read(&mut buffer).unwrap();
+                        println!("Read: {size} bytes from connection");
+                        println!("Read: {:?}", std::str::from_utf8(&buffer[0..size]).unwrap());
+                    },
+                    Err(e) => { eprintln!("Failed to read input due to: {e}"); std::process::exit(0); }
+                }
+            }
+        }
+        Err(e) => { eprintln!("Failed to connect to TCP server due to: {e}"); std::process::exit(1); }
+    }
+}
+
+#[allow(dead_code)]
+fn chat_server() {
+    use std::net::TcpListener;
+    use std::io::{Write, Read};
+
+    let tcp_listener = match TcpListener::bind("127.0.0.1:8000") {
+        Ok(listener) => listener,
+        Err(e) => { eprintln!("Failed to create a TCP Listener due to: {e}"); std::process::exit(1); }
+    };
+
+    println!("Listening on address: {}", tcp_listener.local_addr().unwrap());
+
+    match tcp_listener.accept() {
+        Ok((mut stream, addr)) => {
+            println!("Connection established with: {}", addr);
+            let mut buffer = [0; 100];
+            let mut input = String::new();
+            loop {
+                match stream.read(&mut buffer) {
+                    Ok(size) => {
+                        println!("Read: {size} bytes from connection");
+                        println!("Read: {:?}", std::str::from_utf8(&buffer[0..size]).unwrap());
+
+                        let size = std::io::stdin().read_line(&mut input).unwrap();
+                        println!("Read: {size} bytes of input");
+
+                        let size = stream.write(&input.as_bytes()).unwrap();
+                        println!("Wrote {size} bytes to connection");
+                        input.clear()
+                    },
+                    Err(e) => {
+                        eprintln!("Failed reading from connection due to {e}");
+                        std::process::exit(1);
+                    }
+                }
+            }
+        },
+        Err(e) => { eprintln!("Failed to accept connection due to: {e}"); std::process::exit(1); }
+    }
+}
+
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
+    if args[1] == "-c" { // client
+        chat_client()
+    } else if args[1] == "-s" { // server
+        chat_server()
+    }
 }
