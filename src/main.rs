4994fc052bb13bf39ffa33d95e40ade15f12e7ed
Syed Fasiuddin <66054777+SyedFasiuddin@users.noreply.github.com>
2023-03-23 12:14:53 +0530

    cargo fmt fixes

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,20 @@
 #[allow(dead_code)]
 fn echo() {
-    use std::net::TcpListener;
     use std::io::{Read, Write};
+    use std::net::TcpListener;
 
     let tcp_listener = match TcpListener::bind("127.0.0.1:8000") {
         Ok(listener) => listener,
-        Err(e) => { eprintln!("Failed to create a TCP Listener due to: {e}"); std::process::exit(1); }
+        Err(e) => {
+            eprintln!("Failed to create a TCP Listener due to: {e}");
+            std::process::exit(1);
+        }
     };
 
-    println!("Listening on address: {}", tcp_listener.local_addr().unwrap());
+    println!(
+        "Listening on address: {}",
+        tcp_listener.local_addr().unwrap()
+    );
 
     match tcp_listener.accept() {
         Ok((mut tcp_stream, client_addr)) => {
@@ -17,22 +23,28 @@ fn echo() {
             let _ = tcp_stream.read_to_string(&mut buffer);
             println!("Received data: {buffer}");
             let _ = tcp_stream.write(buffer.as_bytes());
-        },
+        }
         Err(e) => eprintln!("Failed to accept TCP connection due to: {e}"),
     }
 }
 
 #[allow(dead_code)]
 fn echo_continously() {
-    use std::net::TcpListener;
     use std::io::{Read, Write};
+    use std::net::TcpListener;
 
     let tcp_listener = match TcpListener::bind("127.0.0.1:8000") {
         Ok(listener) => listener,
-        Err(e) => { eprintln!("Failed to create a TCP Listener due to: {e}"); std::process::exit(1); }
+        Err(e) => {
+            eprintln!("Failed to create a TCP Listener due to: {e}");
+            std::process::exit(1);
+        }
     };
 
-    println!("Listening on address: {}", tcp_listener.local_addr().unwrap());
+    println!(
+        "Listening on address: {}",
+        tcp_listener.local_addr().unwrap()
+    );
 
     for stream in tcp_listener.incoming() {
         match stream {
@@ -44,13 +56,13 @@ fn echo_continously() {
                         println!("Read {size} bytes from {peer_addr}");
                         println!("Msg: {:?}", std::str::from_utf8(&buffer[0..size]).unwrap());
                         let _ = stream.write(&buffer[0..size]);
-                    },
+                    }
                     Err(e) => {
                         eprintln!("Error reading from ({peer_addr}) due to: {e}");
                         std::process::exit(1);
                     }
                 }
-            },
+            }
             Err(e) => eprintln!("Failed to accept TCP connection due to: {e}"),
         }
     }
@@ -58,20 +70,26 @@ fn echo_continously() {
 
 #[allow(dead_code)]
 fn socket_file() {
-    use std::os::unix::net::UnixListener;
     use std::io::Read;
+    use std::os::unix::net::UnixListener;
 
     let listener = match UnixListener::bind("/tmp/socket-rs-hello.socket") {
         Ok(socket) => socket,
-        Err(e) => { eprintln!("Failed to create the socket due to: {e}"); std::process::exit(1); }
+        Err(e) => {
+            eprintln!("Failed to create the socket due to: {e}");
+            std::process::exit(1);
+        }
     };
 
     let mut buffer = [0; 20];
     match listener.accept() {
-        Ok((mut socket, addr)) => {
-            match socket.read(&mut buffer) {
-                Ok(size) => print!("Size: {size}\nMsg: {}", String::from_utf8(buffer.to_vec()).unwrap()),
-                Err(e) => eprintln!("Failed to read from socket (addr: {addr:?}) into buffer due to: {e}"),
+        Ok((mut socket, addr)) => match socket.read(&mut buffer) {
+            Ok(size) => print!(
+                "Size: {size}\nMsg: {}",
+                String::from_utf8(buffer.to_vec()).unwrap()
+            ),
+            Err(e) => {
+                eprintln!("Failed to read from socket (addr: {addr:?}) into buffer due to: {e}")
             }
         },
         Err(e) => eprintln!("Failed to accept connection due to: {e}"),
@@ -80,8 +98,8 @@ fn socket_file() {
 
 #[allow(dead_code)]
 fn chat_client() {
+    use std::io::{Read, Write};
     use std::net::TcpStream;
-    use std::io::{Write, Read};
 
     match TcpStream::connect("127.0.0.1:8000") {
         Ok(mut stream) => {
@@ -100,26 +118,38 @@ fn chat_client() {
                         let size = stream.read(&mut buffer).unwrap();
                         println!("Read: {size} bytes from connection");
                         println!("Read: {:?}", std::str::from_utf8(&buffer[0..size]).unwrap());
-                    },
-                    Err(e) => { eprintln!("Failed to read input due to: {e}"); std::process::exit(0); }
+                    }
+                    Err(e) => {
+                        eprintln!("Failed to read input due to: {e}");
+                        std::process::exit(0);
+                    }
                 }
             }
         }
-        Err(e) => { eprintln!("Failed to connect to TCP server due to: {e}"); std::process::exit(1); }
+        Err(e) => {
+            eprintln!("Failed to connect to TCP server due to: {e}");
+            std::process::exit(1);
+        }
     }
 }
 
 #[allow(dead_code)]
 fn chat_server() {
+    use std::io::{Read, Write};
     use std::net::TcpListener;
-    use std::io::{Write, Read};
 
     let tcp_listener = match TcpListener::bind("127.0.0.1:8000") {
         Ok(listener) => listener,
-        Err(e) => { eprintln!("Failed to create a TCP Listener due to: {e}"); std::process::exit(1); }
+        Err(e) => {
+            eprintln!("Failed to create a TCP Listener due to: {e}");
+            std::process::exit(1);
+        }
     };
 
-    println!("Listening on address: {}", tcp_listener.local_addr().unwrap());
+    println!(
+        "Listening on address: {}",
+        tcp_listener.local_addr().unwrap()
+    );
 
     match tcp_listener.accept() {
         Ok((mut stream, addr)) => {
@@ -138,23 +168,28 @@ fn chat_server() {
                         let size = stream.write(input.as_bytes()).unwrap();
                         println!("Wrote {size} bytes to connection");
                         input.clear()
-                    },
+                    }
                     Err(e) => {
                         eprintln!("Failed reading from connection due to {e}");
                         std::process::exit(1);
                     }
                 }
             }
-        },
-        Err(e) => { eprintln!("Failed to accept connection due to: {e}"); std::process::exit(1); }
+        }
+        Err(e) => {
+            eprintln!("Failed to accept connection due to: {e}");
+            std::process::exit(1);
+        }
     }
 }
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
-    if args[1] == "-c" { // client
+    if args[1] == "-c" {
+        // client
         chat_client()
-    } else if args[1] == "-s" { // server
+    } else if args[1] == "-s" {
+        // server
         chat_server()
     }
 }
